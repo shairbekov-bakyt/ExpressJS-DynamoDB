3f949cd1cab8bd0e811bcbe4ed43901d56f2fa77
c0mrade <cyberstalker0726@gmail.com>
2022-12-22 00:30:15 +0600

    feature/added_express_application

---
+++ app.js
@@ -0,0 +1,74 @@
+const {
+    addOrUpdateUser,
+    getUsers,
+    deleteUserById,
+    getUserById,
+} = require('./dynamo');
+const express = require('express')
+const bodyParser = require('body-parser')
+const {response} = require("express");
+const app = express()
+const port = 3000
+
+app.use(bodyParser());
+app.get('/', (req, res) => {
+    res.send('Hello World!').status(200)
+})
+
+app.get('/api/v1/users', async (request, response) => {
+    try {
+        const users = await getUsers()
+        response.send(users).status(200)
+    } catch (err) {
+        response.send({errorMessage: 'Something went wrong'}).status(500)
+    }
+
+})
+
+app.get('/api/v1/users/:id', async (request, response) => {
+    const id = request.params.id
+    try {
+        const userById = await getUserById(id)
+        response.send(userById).status(200)
+    } catch (error) {
+        response.send({errorMessage: 'Something went wrong'}).status(500)
+    }
+})
+
+app.delete('/api/v1/users/:id', async (request, response) => {
+    const id = request.params.id
+    try {
+        await deleteUserById(id);
+        response.send({state: "success"}).status(200)
+
+    } catch (err) {
+        response.send({errorMesage: 'Something went wrong'}).status(500)
+    }
+})
+
+app.post('/api/v1/users', async (request, response) => {
+    const user = request.body;
+    try {
+        const newUser = await addOrUpdateUser(user);
+        response.json(newUser).status(200);
+    } catch (err) {
+        response.status(500).send({errorMessage: 'Something went wrong'});
+    }
+});
+
+app.put('/api/v1/users/:id', async (request, response) => {
+    const user = request.body;
+    const {id} = request.params;
+    user.id = id;
+    try {
+        const newUser = await addOrUpdateUser(user);
+        response.send(newUser).status(200);
+    } catch (err) {
+        response.send({errorMessage: 'Something went wrong'}).status(500);
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
